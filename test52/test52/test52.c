77fc04cf640a9b79d14fd1a0bcc65a1faa0ee3ee
Yzq1335 <1726655631@qq.com>
2024-03-11 18:47:48 +0800

    learning

---
+++ test52/test52/test52.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0; int count=0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9) { count++; }
+//		if (i / 10 == 9) { count++; }
+//	}
+//	printf("%d\n",count);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0; double sum = 0.0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 0)
+//			sum -= 1.0 / i;
+//		else
+//		sum+=1.0 / i;
+//	}printf("%lf\n", sum);
+//	return 0;
+//
+//}
+//int max(int x, int y, int z)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//	if (x > z)
+//		return x;
+//	else
+//		return z;
+//	if (y > z)
+//		return y;
+//	else
+//		return z;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int max(a, b, c);
+//	printf("%d\n", max(a, b, c));
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		if(arr[i]>max)
+//		{
+//			max = arr[i];
+//		}
+//	}printf("%d\n", max);
+//	return 0;
+//
+//}
+//int main() {
+//	int i = 1;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++) {
+//			printf("%d*%d=%-2d", i, j, i * j);
+//		}printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
